f01630e3d94fdba04d11dfd38c305255a28d8f51
tmaquieira <55108908+tmaquieira@users.noreply.github.com>
2021-09-30 10:30:44 -0300

    Initial commit

---
+++ src/modulo1/Ejercicio3.java
@@ -0,0 +1,13 @@
+package modulo1;
+
+public class Ejercicio3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tecla de escape\t\t\tsignificado");
+		System.out.println("\n\\n\t\t\t\tsignifica nueva línea");
+		System.out.println("\\t\t\t\t\tsignifica un tab de espacio");
+		System.out.println("\\”\t\t\t\tes para poner “ (comillas dobles) dentro del texto por ejemplo \n\t\t\t\t“Belencita”");
+		System.out.println("\\\\\t\t\t\tse utiliza para escribir la \\ dentro del texto, por ejemplo \\algo\\");
+		System.out.println("\\’\t\t\t\tse utiliza para las ‘(comilla simple) para escribir por ejemplo ‘Princesita’");
+	}
+}
