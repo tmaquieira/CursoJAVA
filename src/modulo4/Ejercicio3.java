227fe6e600bd20176b099f195461c8bc0832e18d
tmaquieira <55108908+tmaquieira@users.noreply.github.com>
2021-10-12 15:00:40 -0300

    upd

---
+++ src/modulo4/Ejercicio3.java
@@ -0,0 +1,15 @@
+package modulo4;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 for(int i=0; i<11; i++){
+			for(int j=0; j<11; j++){
+				System.out.println(i+" x "+j+" = "+i*j);
+			}
+		}	
+	
+	}
+}
+
