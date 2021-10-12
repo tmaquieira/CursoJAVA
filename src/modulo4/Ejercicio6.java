60483afcd8a16b70384161d6206b81f78c057cc4
tmaquieira <55108908+tmaquieira@users.noreply.github.com>
2021-10-12 17:51:19 -0300

    upd

--- src/modulo4/Ejercicio6.java
+++ src/modulo4/Ejercicio6.java
@@ -33,3 +33,31 @@ public class Ejercicio6 {
 	System.out.println("El sueldo Neto será de: $ "+(sueldoB+numCat+(sueldoB*numAnt)));
 		}
 }
+
+/*CÓDIGO MEJORADO (1 sola variable de cálculo):
+ 
+ System.out.println("Ingrese su categoria...");
+		Scanner sc= new Scanner(System.in);
+		String categoria = sc.next();
+		System.out.println("Ingrese su antiguedad...");
+		int antiguedad = sc.nextInt();
+		System.out.println("Ingrese su sueldo...");
+		double sueldoB = sc.nextDouble();
+
+		if(antiguedad >=1 && antiguedad <=5) {
+			sueldoB = sueldoB * 1.05;
+		} else if (antiguedad >=6 && antiguedad <=10) {
+			sueldoB = sueldoB * 1.10;
+		}else if (antiguedad >10){
+			sueldoB = sueldoB * 1.30;
+		}
+		if(categoria.equals("a")) {
+			sueldoB = sueldoB + 1000.00;
+		} else if (categoria.equals("b")) {
+			sueldoB = sueldoB + 2000.00;
+		}else if (categoria.equals("c")){
+			sueldoB = sueldoB + 3000.00;
+		}
+	System.out.println("El sueldo Neto será de: $ "+sueldoB);
+ 
+ */
--- src/modulo4/Ejercicio7NOentiendoLAconsigna.java
+++ src/modulo4/Ejercicio7NOentiendoLAconsigna.java
@@ -1,41 +1,53 @@
 package modulo4;
 
-import java.util.Scanner;
-
 public class Ejercicio7NOentiendoLAconsigna {
 
 	public static void main(String[] args) {
-		/*(solo para valientes), tomar el ejercicio anterior y realizar un ciclo 
-		 que se repita 10 veces tomando al categorías al azar, lo mismo que el 
-		 sueldo con valores lógicos.
-a. Utilizar Math.random(), y , utilizar un ciclo while y solamente salir de este 
-cuando el resultado este dentro de los correctos.
-*/		
 		// TODO Auto-generated method stub
-		System.out.println("Ingrese su categoria...");
-		Scanner sc= new Scanner(System.in);
-		String categoria = sc.next();
-		System.out.println("Ingrese su antiguedad...");
-		int antiguedad = sc.nextInt();
-		System.out.println("Ingrese su sueldo...");
-		double sueldoB = sc.nextDouble();
-		double numAnt = 0.00;
-		double numCat = 0.00;
+		/*realizar un ciclo FOR de 10 repeticiones que tome Categorías, antiguedades 
+		 y Sueldos al azar, con valores lógicos. Utilizar Math.random(), 
+		 con un ciclo while y solo salir de este si el 
+		 resultado es correcto.*/
 
-		if(antiguedad >=1 && antiguedad <=5) {
-			numAnt = 0.05;
-		} else if (antiguedad >=6 && antiguedad <=10) {
-			numAnt = 0.10;
-		}else if (antiguedad >10){
-			numAnt = 0.30;
+		String categorias = "abc";
+	
+		for (int i = 0; i < 10; i++) {
+	
+			int categoriaAlAzar = (int) (Math.random()*3);
+			char categoriaAlAzarATexto = categorias.charAt(categoriaAlAzar);
+			int antiguedadAlAzar = (int) (Math.random()*35);
+			double sueldoAlAzar = Math.floor(Math.random()*100000);
+
+			System.out.println("\nEmpleado Nro: "+i);
+			System.out.println("Categoría: "+categoriaAlAzarATexto);
+			System.out.println("Antiguedad: "+antiguedadAlAzar);
+			
+			if(categoriaAlAzar == 0) {
+				sueldoAlAzar = sueldoAlAzar + 1000.00;
+			} else if (categoriaAlAzar == 1) {
+				sueldoAlAzar = sueldoAlAzar + 2000.00;
+			}else if (categoriaAlAzar == 2){
+				sueldoAlAzar = sueldoAlAzar + 3000.00;
+			}
+			if(antiguedadAlAzar >=1 && antiguedadAlAzar <=5) {
+				sueldoAlAzar = sueldoAlAzar * 1.05;
+			} else if (antiguedadAlAzar >=6 && antiguedadAlAzar <=10) {
+				sueldoAlAzar = sueldoAlAzar * 1.10;
+			}else if (antiguedadAlAzar >10){
+				sueldoAlAzar = sueldoAlAzar * 1.30;
+			}	
+			
+			while (sueldoAlAzar > 0){
+				System.out.println("\nEl sueldo Neto será de: $ "+(double)(Math.round(sueldoAlAzar * 100) / 100));
+				break; 
+			//el break es para frenar la repetición. 
+			//el Math.round para mostrar solo 2 decimales.
+			}
+			
 		}
-		if(categoria.equals("a")) {
-			numCat = 1000.00;
-		} else if (categoria.equals("b")) {
-			numCat = 2000.00;
-		}else if (categoria.equals("c")){
-			numCat = 3000.00;
-		}
-	System.out.println("El sueldo Neto será de: $ "+(sueldoB+numCat+(sueldoB*numAnt)));
-		}
+
+		
+	}
 }
+
+
