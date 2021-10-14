b4c0e1af34c5edb7972d398b175206ede09e2ce9
tmaquieira <55108908+tmaquieira@users.noreply.github.com>
2021-10-14 15:25:32 -0300

    upd

--- src/modulo3/Ejercicio11.java
+++ src/modulo3/Ejercicio11.java
@@ -8,18 +8,14 @@ public class Ejercicio11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ingrese su número de Ruleta...");
+		System.out.println("Ingrese su letra...");
 		Scanner sc= new Scanner(System.in);
-		int numeroRuleta = sc.nextInt();
-				
-		if (numeroRuleta>=1 && numeroRuleta<=12){
-			System.out.println(numeroRuleta+" pertenece a la Primera Docena.");
-		}else if (numeroRuleta>=13 && numeroRuleta<=24){
-			System.out.println(numeroRuleta+" pertenece a la Segunda Docena.");
-		}else if (numeroRuleta>=25 && numeroRuleta<=36){
-				System.out.println(numeroRuleta+" pertenece a la Tercera Docena.");
-		}else if (numeroRuleta<1 || numeroRuleta>36) {
-		System.out.println("El número "+numeroRuleta+" está fuera de rango.");
+		char letra = sc.next().charAt(0);
+
+		if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
+			System.out.println("Es una vocal");
+		}else {
+			System.out.println("No es una vocal");
+		}
 	}
-}
 }
--- src/modulo3/Ejercicio12.java
+++ src/modulo3/Ejercicio12.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class Ejercicio12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Ingrese su letra...");
+		System.out.println("Ingrese su número de Ruleta...");
 		Scanner sc= new Scanner(System.in);
-		String letra = sc.next();
-
-		if (letra.equals("a") || letra.equals("e") || letra.equals("i") || letra.equals("o") || letra.equals("u")) {
-			System.out.println("Es una vocal");
-		}else {
-			System.out.println("No es una vocal");
+		int numeroRuleta = sc.nextInt();
+				
+		if (numeroRuleta>=1 && numeroRuleta<=12){
+			System.out.println(numeroRuleta+" pertenece a la Primera Docena.");
+		}else if (numeroRuleta>=13 && numeroRuleta<=24){
+			System.out.println(numeroRuleta+" pertenece a la Segunda Docena.");
+		}else if (numeroRuleta>=25 && numeroRuleta<=36){
+				System.out.println(numeroRuleta+" pertenece a la Tercera Docena.");
+		}else if (numeroRuleta<1 || numeroRuleta>36) {
+		System.out.println("El número "+numeroRuleta+" está fuera de rango.");
 		}
 	}
 }
